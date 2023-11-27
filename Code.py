848604defdba9d6e8603f3ad4b22ffeb86c3e338
amirhanii <145559343+amirhanii@users.noreply.github.com>
2023-11-27 18:01:55 +0200

    Update Code.py
    encryption

--- Code.py
+++ Code.py
@@ -1,6 +1,58 @@
 def text_to_morse(character):
-    if character == "A":
+    if character == 'A':
         return ".-"
-    if character == "B":
+    elif character == 'B':
         return "-..."
+    elif character == 'C':
+        return "-.-."
+    elif character == 'D':
+        return "-.."
+    elif character == 'E':
+        return "."
+    elif character == 'F':
+        return "..-."
+    elif character == 'G':
+        return "--."
+    elif character == 'H':
+        return "...."
+    elif character == 'I':
+        return ".."
+    elif character == 'J':
+        return ".---"
+    elif character == 'K':
+        return "-.-"
+    elif character == 'L':
+        return ".-.."
+    elif character == 'M':
+        return "--"
+    elif character == 'N':
+        return "-."
+    elif character == 'O':
+        return "---"
+    elif character == 'P':
+        return ".--."
+    elif character == 'Q':
+        return "--.-"
+    elif character == 'R':
+        return ".-."
+    elif character == 'S':
+        return "..."
+    elif character == 'T':
+        return "-"
+    elif character == 'U':
+        return "..-"
+    elif character == 'V':
+        return "...-"
+    elif character == 'W':
+        return ".--"
+    elif character == 'X':
+        return "-..-"
+    elif character == 'Y':
+        return "-.--"
+    elif character == 'Z':
+        return "--.."  
+
+ 
+    
+    
     
